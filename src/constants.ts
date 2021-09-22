6444b4ccd5ec75c19720e8d5c6e95cd8a353bb33
Sergey Mirzoyan <smirzoyan@cloudchipr.com>
2021-09-22 20:33:04 +0200

    Add constants (#12)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,23 @@
+export enum CloudProvider {
+    AWS = 'aws',
+}
+
+export enum Region {
+    US_EAST_1 = 'us-east-1',
+}
+
+export enum Profile {
+    DEFAULT = 'default',
+}
+
+export enum Output {
+    DETAILED = 'detailed',
+    SUMMARIZED = 'summarized',
+}
+
 export enum OutputFormats {
     JSON = 'json',
     YAML = 'yaml',
     TABLE = 'table',
-    TEXT = 'text'
+    TEXT = 'text',
 }
--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,21 @@
 import { Command, Option } from 'commander';
 import AwsCliManager from "./aws/aws-cli-manager";
 import { OutputService } from './services/output/output-service';
+import {CloudProvider, Output, OutputFormats, Profile, Region} from "./constants";
 
 const program = new Command();
 
 const collect = program
-  .addOption(new Option('--cloud-provider <cloud-provider>', 'Cloud provider').default('aws'))
-  .addOption(new Option('--region <region>', 'Region').default('us-east-1'))
+  .addOption(new Option('--cloud-provider <cloud-provider>', 'Cloud provider').default(CloudProvider.AWS))
+  .addOption(new Option('--region <region>', 'Region').default(Region.US_EAST_1))
   .addOption(new Option('--account-id <account-id>', 'Account id'))
   .addOption(new Option('--verbose <verbose>', 'Verbose').default(0))
-  .addOption(new Option('--profile <profile>', 'Profile').default('default'))
+  .addOption(new Option('--profile <profile>', 'Profile').default(Profile.DEFAULT))
   .addOption(new Option('--version <version>', 'Version'))
   .addOption(new Option('--dry-run <dry-run>', 'Dry run'))
   .addOption(new Option('--help <help>', 'Help'))
-  .addOption(new Option('--output <output>', 'Output').default('detailed').choices(['summarized', 'detailed']))
-  .addOption(new Option('--output-format <output-format>', 'Output format').default('text').choices(['json', 'text', 'table', 'yaml']))
+  .addOption(new Option('--output <output>', 'Output').default(Output.DETAILED).choices(Object.values(Output)))
+  .addOption(new Option('--output-format <output-format>', 'Output format').default(OutputFormats.TEXT).choices(Object.values(OutputFormats)))
   .command('collect');
 
 collect
