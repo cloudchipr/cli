b39e90be2e2056979cf1458257d099a6ecffa9a7
Suren Marashlyan <marashlyan.suren@gmail.com>
2021-09-30 16:32:00 +0200

    Add command decorator, and separate aws logic (#21)
    * Add command decorator, and separate aws logic
    
    * Redesign decorator to cli customization functional
    
    * remove use less line

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,36 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
-import {Command, Option, OptionValues} from 'commander';
-import EngineRequestBuilder from "./engine-request-builder";
-import {Command as CloudChiprCommand} from "cloudchipr-engine/lib/Command";
-import {AwsSubCommand} from "cloudchipr-engine/lib/aws-sub-command";
-import {AWSShellEngineAdapter} from "cloudchipr-engine/lib/adapters/aws-shell-engine-adapter";
-import {EbsResponse} from "cloudchipr-engine/lib/responses/ebs-response";
-import { OutputService } from './services/output/output-service';
-import {CloudProvider, Output, OutputFormats, Profile, Region} from "./constants";
 import chalk from 'chalk';
+import {Command, Option} from "commander";
+import {CloudProvider, Output, OutputFormats, Region} from "./constants";
+import CloudChiprCliProvider from "./cli/cloud-chipr-cli-provider";
 
-const program = new Command();
+const command = new Command();
+command
+    .addOption(new Option('--cloud-provider <cloud-provider>', 'Cloud provider').default(CloudProvider.AWS).choices(Object.values(CloudProvider)))
+    .addOption(new Option('--region <region>', 'Region').default(Region.US_EAST_1))
+    .addOption(new Option('--verbose <verbose>', 'Verbose').default(0))
+    .addOption(new Option('--version <version>', 'Version'))
+    .addOption(new Option('--dry-run <dry-run>', 'Dry run'))
+    .addOption(new Option('--help <help>', 'Help'))
+    .addOption(new Option('--output <output>', 'Output').default(Output.DETAILED).choices(Object.values(Output)))
+    .addOption(new Option('--output-format <output-format>', 'Output format').default(OutputFormats.TEXT).choices(Object.values(OutputFormats)));
 
-const collect = program
-  .addOption(new Option('--cloud-provider <cloud-provider>', 'Cloud provider').default(CloudProvider.AWS).choices(Object.values(CloudProvider)))
-  .addOption(new Option('--region <region>', 'Region').default(Region.US_EAST_1))
-  .addOption(new Option('--account-id <account-id>', 'Account id'))
-  .addOption(new Option('--verbose <verbose>', 'Verbose').default(0))
-  .addOption(new Option('--profile <profile>', 'Profile').default(Profile.DEFAULT))
-  .addOption(new Option('--version <version>', 'Version'))
-  .addOption(new Option('--dry-run <dry-run>', 'Dry run'))
-  .addOption(new Option('--help <help>', 'Help'))
-  .addOption(new Option('--output <output>', 'Output').default(Output.DETAILED).choices(Object.values(Output)))
-  .addOption(new Option('--output-format <output-format>', 'Output format').default(OutputFormats.TEXT).choices(Object.values(OutputFormats)))
-  .command('collect');
-
+const collect = command.command('collect');
 collect
-  .command('all')
-  .option('-f, --filter <type>', 'Filter')
-  .action((options) => {
-  });
-
-collect
-    .command('ebs')
+    .command('all')
     .option('-f, --filter <type>', 'Filter')
     .action((options) => {
-        const output = new OutputService();
-        const request = EngineRequestBuilder
-            .builder()
-            .setOptions(Object.assign(program.opts(), options) as OptionValues)
-            .setCommand(CloudChiprCommand.collect())
-            .setSubCommand(AwsSubCommand.ebs())
-            .build();
-
-        const engineAdapter = new AWSShellEngineAdapter(process.env.C8R_CUSTODIAN as string)
-        let response = engineAdapter.execute<EbsResponse>(request)
-
-        output.print(response.items, program.opts().outputFormat)
     });
 
-collect
-  .command('ec2')
-  .option('-f, --filter <type>', 'Filter')
-  .action((options) => {
-    const output = new OutputService();
-    output.print(program.opts().region, program.opts().outputFormat);
-    console.log('collect ec2');
-  });
+const cloudChiprCli = CloudChiprCliProvider.getProvider(command.opts().cloudProvider);
+cloudChiprCli
+    .customiseCommand(command)
+    .customiseCollectCommand(collect);
 
 try {
-    program.parse(process.argv);
+    command.parse(process.argv);
 } catch (e) {
     console.error(chalk.red(chalk.underline('Error:'), e.message));
 }
