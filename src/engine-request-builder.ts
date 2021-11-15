a8c7b7498744a1b4e39990088dc0e636214c4763
smarashlyan <marashlyan.suren@gmail.com>
2021-11-16 00:22:09 +0100

    Remove aws sdk, and aws credential manager
    # Conflicts:
    #	src/cli/aws-cloud-chipr-cli.ts
    #	src/engine-request-builder.ts

--- src/engine-request-builder.ts
+++ src/engine-request-builder.ts
@@ -2,11 +2,9 @@ import {
   EngineRequest,
   Command,
   SubCommandInterface,
-  Parameter,
-  Configuration
+  Parameter
 } from '@cloudchipr/cloudchipr-engine'
 import { OptionValues } from 'commander'
-import CredentialProvider from './credential-provider'
 import { FilterProvider } from './filter-provider'
 
 export default class EngineRequestBuilder {
@@ -31,7 +29,6 @@ export default class EngineRequestBuilder {
 
     build (): EngineRequest {
       return new EngineRequest(
-        EngineRequestBuilder.buildConfiguration(this.options),
         this.command,
         this.subCommand,
         EngineRequestBuilder.buildParameter(this.options)
@@ -40,13 +37,6 @@ export default class EngineRequestBuilder {
 
     public static builder (): EngineRequestBuilder {
       return new EngineRequestBuilder()
-    }
-
-    private static buildConfiguration (options: OptionValues): Configuration {
-      const credentialProvider = new CredentialProvider()
-      const credentials = credentialProvider.getCredentials(options.cloudProvider, options)
-
-      return new Configuration(credentials.getAccessKey(), credentials.getSecretKey(), options.region)
     }
 
     private static buildParameter (options: OptionValues): Parameter {
