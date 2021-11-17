049de726dfdfb9de8f898db794692d4d498d3477
Tigran Sardaryan <tig.sardaryan@gmail.com>
2021-11-17 23:34:01 +0100

    Add collect All command

--- src/responses/collect-all-summary-decorator.ts
+++ src/responses/collect-all-summary-decorator.ts
@@ -1,4 +1,4 @@
-import { Alb, Ebs, Ec2, Eip, Elb, Nlb, ProviderResource, Rds, Response } from '../../../cloudchipr-engine'
+import { ProviderResource, Response } from '../../../cloudchipr-engine'
 
 export default class CollectAllSummaryDecorator {
   decorate (resources: Array<Response<ProviderResource>>) {
@@ -10,55 +10,9 @@ export default class CollectAllSummaryDecorator {
   }
 
   private static eachItem (resource: Array<ProviderResource>) {
-    return CollectAllSummaryDecorator[resource[0].constructor.name.toLowerCase()](resource)
-  }
-
-  private static ec2 (ec2: Array<Ec2>) {
     return {
-      service: 'EC2',
-      costPerMonth: ec2.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
-    }
-  }
-
-  private static ebs (ebs: Array<Ebs>) {
-    return {
-      service: 'EBS',
-      costPerMonth: ebs.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
-    }
-  }
-
-  private static rds (rds: Array<Rds>) {
-    return {
-      service: 'RDS',
-      costPerMonth: rds.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
-    }
-  }
-
-  private static eip (eip: Array<Eip>) {
-    return {
-      service: 'EIP',
-      costPerMonth: eip.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
-    }
-  }
-
-  private static elb (elb: Array<Elb>) {
-    return {
-      service: 'ELB',
-      costPerMonth: elb.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
-    }
-  }
-
-  private static nlb (nlb: Array<Nlb>) {
-    return {
-      service: 'NLB',
-      costPerMonth: nlb.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
-    }
-  }
-
-  private static alb (alb: Array<Alb>) {
-    return {
-      service: 'ALB',
-      costPerMonth: alb.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
+      service: resource[0].constructor.name.toUpperCase(),
+      costPerMonth: resource.map(o => o.pricePerMonth).reduce((a, b) => a + b, 0)
     }
   }
 
