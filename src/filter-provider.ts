aa06ecc26b29e57fe0391cf2bfa9c69ff0fd58f1
Tigran Sardaryan <tig.sardaryan@gmail.com>
2021-11-20 12:18:23 +0100

    Improve Filter Validation (#131)

--- src/filter-provider.ts
+++ src/filter-provider.ts
@@ -1,11 +1,11 @@
 import { readFileSync } from 'fs'
 import yaml from 'yaml'
-import { FilterBuilder, FilterInterface } from '@cloudchipr/cloudchipr-engine'
+import { FilterBuilder, FilterInterface, SubCommandInterface, FilterValidator } from '@cloudchipr/cloudchipr-engine'
 import { OptionValues } from 'commander'
 
 export class FilterProvider {
-  getFilter (options: OptionValues): FilterInterface {
-    const builder = new FilterBuilder()
+  getFilter (options: OptionValues, subCommand: SubCommandInterface): FilterInterface {
+    const builder = new FilterBuilder(new FilterValidator(subCommand))
     return builder
       .load(yaml.parse(readFileSync(options.filter, 'utf8')))
       .toList()
