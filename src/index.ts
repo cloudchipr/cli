b57dee07c267ca48f6bc014363a37f7d168c4f9a
Sergey Mirzoyan <smirzoyan@cloudchipr.com>
2021-12-23 17:00:17 +0400

    Remove filter option from collect and clean all commands (#215)

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ command
   .addOption(new Option('--cloud-provider <cloud-provider>', 'Cloud provider').default(CloudProvider.AWS).choices(Object.values(CloudProvider)))
   .addOption(new Option('--verbose', 'Verbose'))
   .addOption(new Option('--version <version>', 'Version'))
-  .addOption(new Option('--dry-run <dry-run>', 'Dry run'))
   .addOption(new Option('--output <output>', 'Output').default(null).choices(Object.values(Output)))
   .addOption(new Option('--output-format <output-format>', 'Output format').default(OutputFormats.TABLE).choices(Object.values(OutputFormats)))
   .showSuggestionAfterError()
